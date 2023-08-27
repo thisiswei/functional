52d24af80f1d6ec4d91f48c2058cd12f39b106b6
w <w@email>
2023-08-27 13:30:49 -0400

    heap2

--- functionaldatastructure/ls.ml
+++ functionaldatastructure/ls.ml
@@ -83,3 +83,35 @@ fun findMin E = raise Empty
 
 fun deleteMin E = raise Empty
   | deleteMin (T (_, x, a, b)) = merge(a, b)
+
+
+datatype 'a Tree = Node of 'a * 'a * 'a Tree list
+type Heap = int Tree list
+
+fun link (t1 as Node (r, x1, c1), t2 as Node (_, x2, c2)) =
+    if x1 <= x2 then Node (r+1, x1, t2::c1)
+    else Node (r+1, x2, t1::c2)
+
+fun root (Node (r, x, c)) = x
+fun rank (Node (r, x, c)) = r
+fun insTree (t, []) = [t]
+  | insTree (t, ts as t'::ts') =
+      if rank t < rank t' then t::ts else insTree (link (t, t'), ts')
+
+fun insert(x, ts) = insTree(Node (0, x, []), ts)
+fun merge (ts1, []) = ts1
+  | merge ([], ts2) = ts2
+  | merge (ts1 as t1'::ts1', ts2 as t2'::ts2') =
+      if rank t1' < rank t2' then t1' :: merge(ts1', ts2)
+      else if rank t1' > rank t2' then t2' :: merge(ts1, ts2')
+      else insTree (link (t1', t2'), merge(ts1', ts2'))
+
+fun removeMinTree [] = raise Empty
+  | removeMinTree [t] = (t, [])
+  | removeMinTree (t::ts) =
+      let val (t', ts') = removeMinTree ts
+      in
+          if root t' <= root t then (t, ts) else (t', t::ts')
+      end
+
+fun findMin ts = let val (t', ts') = removeMinTree ts in t' end
